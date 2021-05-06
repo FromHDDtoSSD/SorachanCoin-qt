6d50b1c14328b8410622733042d58d05583d0861
FromHDDtoSSD <42310034+FromHDDtoSSD@users.noreply.github.com>
2021-05-06 20:36:18 +0900

    SorachanCoin: improvement CTxDB part75 (finex drive chain)

---
+++ src/bip32/hdchain.cpp
@@ -0,0 +1,51 @@
+
+#include <bip32/hdchain.h>
+#include <crypto/hmac_sha512.h>
+
+static std::vector<std::string> bip39_word_list() {
+    return {"tokyonikonaideitadakitai", "omochi", "kinshu", "itutunoko"
+            "kokorowohitotuni", "iekaradenaide", "tokyokaradenaidekudasai",
+            "kokoonorikirimasho"};
+}
+
+static bool len_check(int8_t size, const std::vector<int8_t> &lens) {
+    for(auto ite: lens) {
+        if(size==ite)
+            return true;
+    }
+    return false;
+}
+
+static uint256 bytes_to_mnemonic(SecureBytes mnemonic_bytes) {
+    if(! len_check(mnemonic_bytes.size(), {16, 20, 24, 28, 32}))
+        throw std::runtime_error("Data length should be one of the following: [16, 20, 24, 28, 32], but it is {len(mnemonic_bytes)}.");
+
+    std::vector<std::string> word_list = bip39_word_list();
+    uint256 key = hash_basis::Hash(mnemonic_bytes.data(), mnemonic_bytes.data()+mnemonic_bytes.size());
+
+    int32_t word_size=0;
+    for(const auto &ite: word_list) {
+        word_size += ite.size();
+    }
+    unsigned char *data = new(std::nothrow) unsigned char[word_size];
+    if(! data)
+        throw std::runtime_error("bytes_to_mnemonic out of memory.");
+    unsigned char *wc=data;
+    for(const auto &ite: word_list) {
+        std::memcpy(wc, ite.c_str(), ite.size());
+        wc+=ite.size();
+    }
+
+    unsigned char buf[64];
+    latest_crypto::CHMAC_SHA512((const unsigned char *)&key, sizeof(key)).Write(data, word_size).Finalize(buf);
+    uint256 hash;
+    std::memcpy(&hash, buf, 32);
+    delete [] data;
+    return hash;
+}
+
+static uint256 generate_mnemonic() {
+    SecureBytes mnemonic_bytes = token_bytes(32);
+    return bytes_to_mnemonic(std::move(mnemonic_bytes));
+}
+
--- src/bip32/hdchain.h
+++ src/bip32/hdchain.h
@@ -8,7 +8,56 @@
 #define BITCOIN_HDCHAIN_H
 
 #include <hash.h>
+#include <cleanse/cleanse.h>
+#include <random/random.h>
 
+class SecureBytes {
+    SecureBytes()=delete;
+    SecureBytes(const SecureBytes &)=delete;
+    SecureBytes &operator=(const SecureBytes &)=delete;
+public:
+    SecureBytes(int8_t sizeIn) {
+        _size=sizeIn;
+        if(_size>0) {
+            _data = new (std::nothrow) unsigned char[_size];
+            if(! _data)
+                throw std::runtime_error("SecureBytes out of memory");
+        }
+    }
+    SecureBytes(SecureBytes &&obj) {
+        operator=(std::move(obj));
+    }
+    SecureBytes &operator=(SecureBytes &&obj) {
+        _size = obj._size;
+        _data = obj._data;
+        obj._size = 0;
+        obj._data = nullptr;
+        return *this;
+    }
+    ~SecureBytes() {
+        if(_size>0) {
+            cleanse::OPENSSL_cleanse(_data, _size);
+            delete [] _data;
+        }
+    }
+    unsigned char *data() {
+        return _data;
+    }
+    int8_t size() {
+        return _size;
+    }
+    operator unsigned char*() {
+        return _data;
+    }
+private:
+    int8_t _size;
+    unsigned char *_data;
+};
 
+static inline SecureBytes token_bytes(int8_t len) {
+    SecureBytes buf(len);
+    latest_crypto::random::GetStrongRandBytes(buf, len);
+    return std::move(buf);
+}
 
 #endif
