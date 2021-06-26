9a60cf62832b5f1f5c61706c837e77a9038add20
FromHDDtoSSD <42310034+FromHDDtoSSD@users.noreply.github.com>
2021-06-26 21:40:44 +0900

    Sora neko: under development Proof of Benchmark part129

--- src/test/key_test.cpp
+++ src/test/key_test.cpp
@@ -16,6 +16,23 @@ public:
         }
         return str;
     }
+    static std::string ToString(const CPubKey::ecmult::secp256k1_ge &obj) {
+        const uint32_t *x = &obj.x.n[0];
+        const uint32_t *y = &obj.y.n[0];
+        std::string str = "\nX: ";
+        for(int i=0; i<10; ++i) {
+            char tmp[32];
+            ::sprintf(tmp, "0x%X", x[i]);
+            str += tmp;
+        }
+        str += "\nY: ";
+        for(int i=0; i<10; ++i) {
+            char tmp[32];
+            ::sprintf(tmp, "0x%X", y[i]);
+            str += tmp;
+        }
+        return str;
+    }
     key_test() {
         debugcs::instance() << "key_test" << debugcs::endl();
 
@@ -23,18 +40,28 @@ public:
         int overflow=0;
         int ret=1;
         std::vector<unsigned char> vchRS;
-        vchRS.reserve(64);
-        for(int i=0; i<64; ++i)
-            vchRS.push_back(0xFF);
-        CPubKey::secp256k1_scalar_set_be32(&r, &vchRS[0], &overflow);
+        vchRS.reserve(65);
+        vchRS.push_back(0x04);
+        for(int i=1; i<65; ++i)
+            vchRS.push_back(0x10);
+
+        CPubKey::secp256k1_scalar_set_be32(&r, &vchRS[1], &overflow);
         ret &= !overflow;
-        CPubKey::secp256k1_scalar_set_be32(&s, &vchRS[32], &overflow);
+        CPubKey::secp256k1_scalar_set_be32(&s, &vchRS[33], &overflow);
         ret &= !overflow;
 
+        ::fprintf(stdout, "k1_scalar_set_be32 overflow: %d\n", ret);
         ::fprintf(stdout, "r: %s\ns: %s\n", ToString(r).c_str(), ToString(s).c_str());
 
         CPubKey::secp256k1_ecdsa_recoverable_signature ers;
         CPubKey::secp256k1_ecdsa_recoverable_signature_save(&ers, &r, &s, 0);
+
+        CPubKey::ecmult::secp256k1_ge Q; // include [x, y] each vector size uint32_t[10] (_fe)
+        CPubKey::secp256k1_eckey_pubkey_parse(&Q, &vchRS[0], 65);
+        ::fprintf(stdout, "ge: %s\n", ToString(Q).c_str());
+
+        int ret2 = CPubKey::ecmult::secp256k1_ge_is_valid_var(&Q);
+        ::fprintf(stdout, "get_is_valid_var: %d\n", ret2);
 
         util::Sleep(10000);
     }
