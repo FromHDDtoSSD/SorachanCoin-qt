9358ab5bd7dd2f7bd998087ff674f47cf29027c1
FromHDDtoSSD <42310034+FromHDDtoSSD@users.noreply.github.com>
2024-07-31 17:23:05 +0900

    Released v3.89.15. Added checkpoints.

--- SorachanCoinQ-qt.pro
+++ SorachanCoinQ-qt.pro
@@ -1,5 +1,5 @@
 TEMPLATE = app
-VERSION = 3.89.14
+VERSION = 3.89.15
 
 INCLUDEPATH += src src/json src/qt
 QT += core gui network
--- src/version.h
+++ src/version.h
@@ -85,6 +85,6 @@ public:
 // display version
 #define DISPLAY_VERSION_MAJOR        3
 #define DISPLAY_VERSION_MINOR        89
-#define DISPLAY_VERSION_REVISION     14
+#define DISPLAY_VERSION_REVISION     15
 
 #endif
